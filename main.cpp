e5a76e30b80ad961c8561edda245a717e6930038
xierhacker <xierhacker@163.com>
2017-08-15 21:34:23 +0800

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,50 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <map>
 #include <algorithm>
-#include <cctype>
+
 using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-//比较函数的写法
-int compare(const pair<char,int> &x,const pair<char,int> &y)
+using namespace std;
+/*bool ascent(int x,int y)
 {
-    if(x.second==y.second)
-        return (x.first<y.first);
-    else
-        return (x.second>y.second);
-}
-
+    return x<y;
+}*/
+/*
+bool descent(int x,int y)
+{
+    return x>y;
+}*/
 int main()
 {
-    string s;
-    map<char,int> m;
-    vector<pair<char,int>> v;
-    cin>>s;
-    for(int i=0;i<s.size();i++)
+    int num;
+    int kind;
+    cin>>num;
+    vector<int> v(num);
+    for(int i=0;i<num;i++)
+        cin>>v[i];
+
+    cin>>kind;
+    sort(v.begin(),v.end());
+    if(kind==0)
     {
-        if(isalpha(s[i])||isalnum(s[i])||isspace(s[i]))
-            //这里注意map的用法
-            m[s[i]]++;
+        for(int i=0;i<num-1;i++)
+            cout<<v[i]<<" ";
+        cout<<v[num-1]<<endl;
     }
 
-    //map中元素转移到vector中
-    for(auto it=m.begin();it!=m.end();it++)
+    else
     {
-        //cout<<it->first<<":"<<it->second<<endl;
-        //注意怎么构造pair
-        v.push_back(make_pair(it->first,it->second));
+        for(int i=v.size()-1;i>0;i--)
+            cout<<v[i]<<" ";
+        cout<<v[0]<<endl;
     }
-
-
-    //排序
-    sort(v.begin(),v.end(),compare);
-
-    for(auto it=v.begin();it!=v.end();it++)
-    {
-        cout<<it->first;
-    }
-    cout<<endl;
     return 0;
 }
-
