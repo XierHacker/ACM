ea365ab6a77bf6d7d330be124972c043b84521f0
xierhacker <xierhacker@163.com>
2019-10-24 12:45:50 +0800

    update

--- 2.Tree/source/binarySearchTree.cpp
+++ 2.Tree/source/binarySearchTree.cpp
@@ -5,30 +5,24 @@
 #include "binarySearchTree.h"
 
 //二叉搜索树转双向链表，并且返回头结点
+//二叉搜索树，注定采用中序遍历
 //二叉搜索树结点之间的转换
 void convertNode(BSTNode* root,BSTNode** LastNodeInList)
 {
-    //二叉搜索树，注定采用中序遍历
-    //边界判断
-    if(root==nullptr)
-        return nullptr;
+    if(root==nullptr)   {return nullptr;}       //边界判断
 
-    //设立一个指针指向当前节点
-    BSTNode* current=root;
+    BSTNode* current=root;      //设立一个指针指向当前节点
 
     //左
-    if(current->lchild!=nullptr)
-        convertNode(current->lchild,LastNodeInList);
+    if(current->lchild!=nullptr)    {convertNode(current->lchild,LastNodeInList);}
 
-    //中
+    //中(这个时候左子树应该已经遍历完了)
     current->lchild=*LastNodeInList;
-    if(*LastNodeInList!=nullptr)
-        (*LastNodeInList)->rchild=current;
+    if(*LastNodeInList!=nullptr)    {(*LastNodeInList)->rchild=current;}
     *LastNodeInList=current;
 
     //右
-    if(current->rchild!=nullptr)
-        convertNode(current->rchild,LastNodeInList);
+    if(current->rchild!=nullptr)    {convertNode(current->rchild,LastNodeInList);}
 }
 
 BSTNode* convertToList(BSTNode* root)
